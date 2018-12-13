f32535826639a9db68d3cc664ca4e53c39f83a8e
Ivan <ivanakazver3d@gmail.com>
2018-12-13 20:05:52 +0700

    Initial commit. Very basic setup for bookmarking app

---
+++ src/renderer/app.js
@@ -0,0 +1,11 @@
+// Main renderer js file
+
+// Show add-modal
+$('.open-add-modal').click(() => {
+    $('#add-modal').addClass('is-active');
+});
+
+// Close add-modal
+$('.close-add-modal').click(() => {
+    $('#add-modal').removeClass('is-active');
+});
